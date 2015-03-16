283d5ddd24f886371393e653ff722770af675e66
Cristian Costa <Cristiancosta1991@gmail.com>
2015-03-16 18:24:26 -0300

    Update GameCircle.hx

--- extension/gc/GameCircle.hx
+++ extension/gc/GameCircle.hx
@@ -34,26 +34,5 @@ class GameCircle {
 		trace("Initializing");
 		javaInit();
 	}
-
-	public static var displayMessage(default,null):Void->String=
-	#if android
-		openfl.utils.JNI.createStaticMethod("com/gcex/GameCircle", "displayMessage", "()Ljava/lang/String;");
-	#else
-		function():String{return "Nada.";}
-	#end
-
-	public static var isInitialized(default,null):Void->Bool=
-	#if android
-		openfl.utils.JNI.createStaticMethod("com/gcex/GameCircle", "isInitialized", "()Z");
-	#else
-		function():Bool{return false;}
-	#end
-
-	public static var init(default,null):Void->Void=
-	#if android
-		openfl.utils.JNI.createStaticMethod("com/gcex/GameCircle", "init", "()V");
-	#else
-		function():Void{}
-	#end
 	
 }
