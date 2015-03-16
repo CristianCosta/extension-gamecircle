d37044d9306da1fdd3a8a592ed57dea6739ec55f
Cristian Costa <cristiancosta1991@gmail.com>
2015-03-16 17:56:14 -0300

    Added Init and Pause methods.

--- dependencies/gcex/src/com/gcex/GameCircle.java
+++ dependencies/gcex/src/com/gcex/GameCircle.java
@@ -8,13 +8,17 @@ import android.app.Activity;
 import org.haxe.extension.Extension;
 import org.haxe.lime.HaxeObject;
 
-import com.amazon.ags.api.player.RequestPlayerResponse;
 import com.amazon.ags.api.AGResponseCallback;
 import com.amazon.ags.api.AmazonGamesClient;
 import com.amazon.ags.api.AmazonGamesFeature;
 import com.amazon.ags.api.AmazonGamesStatus;
 import com.amazon.ags.api.AmazonGamesCallback;
+import com.amazon.ags.api.RequestResponse;
+import com.amazon.ags.api.achievements.GetAchievementsResponse;
+import com.amazon.ags.api.achievements.Achievement;
+import com.amazon.ags.api.leaderboards.GetPlayerScoreResponse;
 import com.amazon.ags.api.overlay.PopUpLocation;
+import com.amazon.ags.constants.LeaderboardFilter;
 
 import java.util.EnumSet;
 
@@ -22,24 +26,74 @@ public class GameCircle extends Extension {
 	
 	private static final String TAG = "EXTENSION-GAMECIRCLE";
 	private static AmazonGamesStatus gamesStatus = AmazonGamesStatus.INITIALIZING;
-	private AmazonGamesClient agsClient = null;
+	private static AmazonGamesClient agsClient = null;
 
 	private static String estado = "Vacio.";
+	private static int numerito = 0;
+	private static int numerote = 1;
 	
-	public static String displayMessage () {
-		//return "GOLA NUNDO.";
-		return estado;
+	//////////////////////////////////////////////////////////////////////
+	///////////// INIT
+	//////////////////////////////////////////////////////////////////////
+
+	public static void pause() {
+		if (agsClient != null) {
+			agsClient.release();
+		}
+		numerote = numerote * 2;
 	}
-	
+
 	public static void init() {
-		//try {
-			mainActivity.runOnUiThread(new Runnable() {
-				public void run() {
-					AmazonGamesClient.initialize(mainActivity, new AmazonGamesCallback() {
-						@Override
-						public void onServiceReady(AmazonGamesClient amazonGamesClient) {
-							//Esta todo bien.
-							estado = "Cul";
+		numerito++;
+		mainActivity.runOnUiThread(new Runnable() {
+			public void run() {
+				AmazonGamesClient.initialize(mainActivity, new AmazonGamesCallback() {
+					@Override
+					public void onServiceReady(AmazonGamesClient amazonGamesClient) {
+						//Esta todo bien.
+						agsClient = amazonGamesClient;
+						estado = "Successful";
+					}
+					@Override
+					public void onServiceNotReady(AmazonGamesStatus reason) {
+						//Esta todo mal.
+						switch (reason) { 
+						case CANNOT_AUTHORIZE: 
+							estado = "onCreate: CANNOT_AUTHORIZE";
+							break;
+						case CANNOT_BIND: 
+							estado = "onCreate: CANNOT_BIND";
+							break;
+						case CANNOT_INITIALIZE:
+							estado = "onCreate: CANNOT_INITIALIZE";
+							break;
+						case INITIALIZING:
+							estado = "onCreate: INITIALIZING";
+							break;							
+						case INVALID_SESSION:
+							estado = "onCreate: INVALID_SESSION";
+							break;						
+						case NOT_AUTHENTICATED: 
+							estado = "onCreate: NOT_AUTHENTICATED";
+							break;
+						case NOT_AUTHORIZED: 
+							estado = "onCreate: NOT_AUTHORIZED";
+							break;
+						case SERVICE_CONNECTED:
+							estado = "onCreate: SERVICE_CONNECTED";
+							break;							
+						case SERVICE_DISCONNECTED:
+							estado = "onCreate: SERVICE_DISCONNECTED";
+							break;							
+						case SERVICE_NOT_OPTED_IN:
+							estado = "onCreate: SERVICE_NOT_OPTED_IN";
+							break;							
+						} 
+					}
+				}, EnumSet.of(AmazonGamesFeature.Achievements, AmazonGamesFeature.Leaderboards));
+			}
+		});
+	}
 						}
 						@Override
 						public void onServiceNotReady(AmazonGamesStatus reason) {
--- extension/gc/GameCircle.hx
+++ extension/gc/GameCircle.hx
@@ -1,6 +1,39 @@
 package extension.gc;
 
+import haxe.Int64;
+
 class GameCircle {
+
+	public static inline var ACHIEVEMENT_STATUS_LOCKED:Int = 0;
+	public static inline var ACHIEVEMENT_STATUS_UNLOCKED:Int = 1;
+
+	//////////////////////////////////////////////////////////////////////
+	///////////// INIT
+	//////////////////////////////////////////////////////////////////////	
+
+	public static var javaInit(default,null):Void->Void=
+	#if android
+		openfl.utils.JNI.createStaticMethod("com/gcex/GameCircle", "init", "()V");
+	#else
+		function():Void{}
+	#end
+
+	public static var javaPause(default,null):Void->Void=
+	#if android
+		openfl.utils.JNI.createStaticMethod("com/gcex/GameCircle", "pause", "()V");
+	#else
+		function():Void{}
+	#end
+
+	public static function pause() {
+		trace("Pausing");
+		javaPause();
+	}
+
+	public static function init() {
+		trace("Initializing");
+		javaInit();
+	}
 
 	public static var displayMessage(default,null):Void->String=
 	#if android
